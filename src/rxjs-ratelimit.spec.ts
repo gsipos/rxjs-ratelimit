d24bdc6adc36aeece9cb969477b2933fa1df26cf
Gergely Sipos <gergely.sipos@doctusoft.com>
2017-09-15 13:52:09 +0200

    refined tests

--- src/rxjs-ratelimit.spec.ts
+++ src/rxjs-ratelimit.spec.ts
@@ -1,51 +1,50 @@
 import './rxjs-ratelimit';
 import { TestScheduler } from 'rxjs/testing/TestScheduler';
 
-describe('rxjs ratelimit', () => { 
+describe('rxjs ratelimit', () => {
     const assertEqual = (actual, expected) => expect(actual).toEqual(expected);
     let scheduler = new TestScheduler(assertEqual);
 
+    let count: number;
+    let window: number;
     let src: string; //source
     let exp: string; //expected
-    
+
     beforeEach(() => {
         scheduler = new TestScheduler(assertEqual);
     });
 
-    it('rate 1 window 50 ', () => {
-        const count = 1;
-        const window = 50;
-        src = 'aaaa-------------';
-        //50    ----| ----| ----| ----|
-        exp = 'a----a----a----a-';
-
+    const testMarbles = () => { 
         const source = scheduler.createColdObservable(src);
         scheduler.expectObservable(source.rateLimit(count, window, scheduler)).toBe(exp);
         scheduler.flush();
+    };
+
+    it('rate 1 window 50 ', () => {
+        count = 1;
+        window = 50;
+        src = 'abcd-------------';
+        exp = 'a----b----c----d-';
+
+        testMarbles();
     });
 
     it('rate 2 window 40 ', () => {
-        const count = 2;
-        const window = 40;
-        src = 'aaa----------------';
-        //40   ---|---|---|---|
-        exp = 'aa--a--------------';
+        count = 2;
+        window = 40;
+        src = 'abc----------------';
+        exp = 'ab--c--------------';
 
-        const source = scheduler.createColdObservable(src);
-        scheduler.expectObservable(source.rateLimit(count, window, scheduler)).toBe(exp);
-        scheduler.flush();
+        testMarbles();
     });
 
     it('rate 5 window 100 ', () => {
-        const count = 5;
-        const window = 100;
-        src = 'aaaaaa-a------aa----';
-        //40   ---|---|---|---|
-        exp = 'aaaaa-----aa--aa----';
+        count = 5;
+        window = 100;
+        src = 'abcdef-g------aa----';
+        exp = 'abcde-----fg--aa----';
 
-        const source = scheduler.createColdObservable(src);
-        scheduler.expectObservable(source.rateLimit(count, window, scheduler)).toBe(exp);
-        scheduler.flush();
+        testMarbles();
     });
 
 });
